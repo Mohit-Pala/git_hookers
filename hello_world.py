93a067d3f1119c6f585c413dec5f5cab05bc607d
Mohit-Pala <vmohit.pala@gmail.com>
2025-12-04 18:44:52 -0500

    slop

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,9 @@
 import os
+import subprocess
 
-print('slop print')
+
+
+# get git diff
+diff = subprocess.run(['git', 'diff', '--name-only', 'HEAD~1'], capture_output=True, text=True).stdout
+print("slop code written, heres whars changed:")
+print(diff)
