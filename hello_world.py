b6ba1aaf744ad69b0005ef614fb24a08a18d5501
Mohit-Pala <vmohit.pala@gmail.com>
2025-12-04 18:54:52 -0500

    slop

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,3 @@
 import os
 import subprocess
  
-
-# get the diff from the last commit 
-diff = subprocess.check_output(['git', 'diff', 'HEAD^', 'HEAD']).decode('utf-8')
-print("slop code written, heres whars changed:\n", diff)
